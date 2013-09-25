460e62584b5f19970f7c56d2c4f83e1a9edba794
Nathan Montez <nt_montez@yahoo.com>
2013-09-25 08:59:27 -0500

    Modified the DistractionTimer object to include additional functions for retrieving internal objects.

--- app/assets/javascripts/DistractionTimer.js
+++ app/assets/javascripts/DistractionTimer.js
@@ -1,6 +1,27 @@
 
 function DistractionTimer() {
-    
+    /*
+
+        DistractionTimer
+            Encapsulates both the Timer and distractions objects.
+
+        Interface:
+            StartDistractions: readies the distractions and Timer
+                objects for tracking distractions.
+
+            KeyPressHandler: A handler for the KeyPress event to
+                be attached to the text editor.
+
+            BlurHandler: A handler for the Blur event to
+                be attached to the window.
+
+            ReturnTimer: Returns the internal timer.
+
+            ReturnDistractions: Returns the interal distractions.
+
+    */
+
+
     var InternalTimer = new Timer();
     var InternalDistractions = new distractions();
 
@@ -14,8 +35,16 @@ function DistractionTimer() {
         InternalDistractions.end();
     }
 
-    this.LostFocusHandler = function() {
+    this.BlurHandler = function() {
         InternalTimer.pause();
         InternalDistractions.start("lostfocus");
+    }
+
+    this.GetTimer = function() {
+        return IternalTimer;
+    }
+
+    this.GetDistractions = function() {
+        return InternalDistractions;
     }
 }
