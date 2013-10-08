70b1828206a7b652d265e4bf06f8669c925a33b6
David Hahn <dhahn@ctatechs.com>
2013-10-07 20:37:14 -0500

    Modified regex to account for &nbsp.

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -25,13 +25,14 @@ describe Entry do
 
   context "after_save" do
     context "tags that should be found" do
-      [" #exampletag.", "#example_tag!", ">#example-tag<", "#example-tag?","#example-tag," ].each do |tag|
+      [" #exampletag.", "#example_tag!", ">#example-tag<", "#example-tag?","#example-tag,", "#example&nbsp"].each do |tag|
         it "should save tag: #{tag}" do
           entry = Entry.create(content: "test #{tag} test")
-          entry.reload.tags.first.name.should eq tag.gsub(/>|<|\?|\.|!|\s|#|,/, "")
+          entry.reload.tags.first.name.should eq tag.gsub(/>|<|\?|\.|!|\s|#|,|&nbsp/, "")
         end
       end
     end
+
     
     context "tags that should not be found" do
       ["#5", "#tag#tag", "link.com#id", "test#tagtest"].each do |tag|
