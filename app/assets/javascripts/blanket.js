33651ed3b4935eb464986ef5ad691a0c45bf0816
Will <will_weaver15@hotmail.com>
2013-10-10 12:07:32 -0500

    some changes to verbatim and monospace

--- app/assets/javascripts/blanket.js
+++ app/assets/javascripts/blanket.js
@@ -40,20 +40,21 @@ function initTiny() {
         		//verbatim is off being turned on
         		if(!verbatimToggle){
         			ed.execCommand('FormatBlock', false, 'blockquote');
+        			ed.controlManager.get('Verbatim').setActive(true);
         			verbatimToggle = true;
+
         			if(!monospaceToggle){
         				ed.execCommand('FontName', false, 'Andale Mono');
-        				mono = true;
+
         			}
         		}
+
             	//verbatim is on being turned off
             	else{
             		ed.execCommand('FormatBlock', false, 'blockquote');
+            		ed.execCommand('FontName', false, 'Andale Mono');
+            		ed.controlManager.get('Verbatim').setActive(false);
             		verbatimToggle = false;
-            		if(mono){
-            			ed.execCommand('FontName', false, 'Andale Mono');
-            			monospaceToggle = false;
-            		}
             	}          	
         	}
         });
@@ -62,17 +63,23 @@ function initTiny() {
         	title : 'Change to monospace',
         	image : 'M.png',
         	onclick : function(){
+        		var fontBox = ed.controlManager.get('fontselect');
+        		var currentFont = fontBox.items[fontBox.selectedIndex];
 
         		if(!verbatimToggle){
         			//monospace is off being turned on
         			if(!monospaceToggle){
-        				ed.execCommand('FontName', false, 'Andale Mono');
         				monospaceToggle = true;
+        				currentFont = fontBox.items[fontBox.selectedIndex];
+        				ed.controlManager.get('Monospace').setActive(true);
+        				ed.execCommand('FontName', false, 'Andale Mono');
+        				
         			}
         			//monospace is on being turned off
         			else{
-        				ed.execCommand('FontName',false, 'Andale Mono'); 
         				monospaceToggle = false; 
+        				ed.controlManager.get('Monospace').setActive(false); 
+        				ed.execCommand('FontName',false, 'Andale Mono');	
         			}
         		}
             	           		
