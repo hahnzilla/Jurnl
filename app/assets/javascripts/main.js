f76953015a76799a6fee778cbf84b039f08fba91
David Hahn <dhahn@ctatechs.com>
2013-10-22 17:25:48 -0500

    Add routes, action and JSON to retrieve current entry.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
 
-	//to close alerts and notices
-	$('.close').click(function() {
-		$(this).closest('div').hide();
-	});
+  //to close alerts and notices
+  $('.close').click(function() {
+    $(this).closest('div').hide();
+  });
 
-	//fill in editor content with today's post
-	//this isn't working
-	$('#opener').click(function() {
-		// $.ajax({
-		// 	type: "GET",
-		// 	url: "/entries/",
-		// })
-  	tinyMCE.get("entry_content").setContent("appended");
-	});
-	
+  //fill in editor content with today's post
+  //this isn't working
+});
+
+$(document).on("click", '#opener', function() {
+  $.getJSON("/entries/current", function(result){
+    console.log(result.content);
+    tinyMCE.get("entry_content").setContent(result.content);
+  });
+
+  //You still need to add the id of the entry on the page if it exists
 });
