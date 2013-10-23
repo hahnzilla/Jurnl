b516b51740fb40a54ce77d5fa094fe7159613879
Frank Albenesius <frankalbenesius@gmail.com>
2013-10-22 19:40:39 -0500

    Saving entry-id on text editor load to the popupdiv's data-entry-id field. Also made the header at the top of index page a link back to itself.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,16 +4,13 @@ $(document).ready(function(){
   $('.close').click(function() {
     $(this).closest('div').hide();
   });
-
-  //fill in editor content with today's post
-  //this isn't working
 });
 
 $(document).on("click", '#opener', function() {
   $.getJSON("/entries/current", function(result){
-    console.log(result.content);
-    tinyMCE.get("entry_content").setContent(result.content);
+    if(result != null){
+      $('#popUpDiv').attr('data-entry-id', result.id);
+      tinyMCE.get("entry_content").setContent(result.content);
+    }
   });
-
-  //You still need to add the id of the entry on the page if it exists
 });
